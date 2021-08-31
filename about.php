74ac222725cc22bd6fef655932e9ad1df47d43a3
Brishty <52675761+TamannaAhsan@users.noreply.github.com>
2021-08-31 07:28:43 +0600

    Add files via upload

---
+++ about.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="US-en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="description" content="This is a web based food delivery system">
+    
+    <meta name="keywords" content="fooddelight,FOODDELIGHT,Food delight,FOOD DElIGHT,Restaurant,restaurant,web based restaurant">
+    
+
+    <link  rel="stylesheet" href="css\style.css"   />
+    <style>
+         a:link {
+        color:#f4f818d2;
+        background-color: transparent;
+        text-decoration: none;
+      }
+      a:visited {
+        color:rgba(255, 255, 255, 0.849);
+        background-color: transparent;
+        text-decoration: none;
+      }
+      a:hover {
+        color:#33CEFF;
+        background-color: transparent;
+        text-decoration: underline;
+      }
+      a:active {
+        color:#33FFD4;
+        background-color: transparent;
+        text-decoration: underline;
+      }
+      
+        p{
+             font-size: 20px;
+            font-weight:300;
+            color:midnightblue;
+        }
+   
+
+    </style>
+   
+     
+
+    <title>About</title>
+
+    </head>
+    <body >
+        <div class="Contact">
+        <div class="container">
+        
+            <div id="Navigation-bar">
+     
+                <div id="FoodDelightLogo">
+                   <img src="homepagephotos\FinalLogo.PNG" alt="Food Delight Logo">
+                  
+             
+                </div>
+                </div>
+       
+                   <div class="Navi_Links">
+    
+                    <div class="NavigationLinks Text-Align ">
+                        <ul>
+                            <li><a href="index.php">HOME</a></li>
+                            <li><a href="categories.php">CATEGORIES</a></li>
+                            <li><a href="foods.php">FOODS</a></li>
+                            <li><a href="profile.php">PROFILE</a></li>
+                           <li><a href="order.php">ORDER</a></li>
+                           <li><a href="about.php">ABOUT</a></li>
+                            <li><a href="#">LOGOUT</a></li>
+    
+                            </ul>
+                            <div class="clearfix"></div>
+                    </div>
+    
+             </div>
+             </div>
+            
+      
+      <div class="container " >
+      
+        <div>
+          <div style="color:#F39C12 ;font-size:30px;margin-left:500px;margin-top:-50px;">
+
+              About Us
+          </div>
+        
+        <div class="container4">
+        
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet luctus quam vitae sollicitudin. Aliquam quis ipsum felis. Praesent lectus nisi, pretium sit amet dolor at, feugiat convallis est. Duis dictum bibendum sapien, et bibendum quam ultrices non. Cras sit amet vulputate purus. Phasellus dictum scelerisque mi, ac tempor neque tempus at. Aliquam id dolor malesuada, ornare nulla consectetur, placerat augue.</p>
+         
+          <p>Diam ut venenatis tellus in metus vulputate eu. Pretium lectus quam id leo in vitae turpis massa. Pharetra vel turpis nunc eget lorem dolor sed viverra. Orci ac auctor augue mauris augue neque gravida in fermentum. Tempus urna et pharetra pharetra massa massa ultricies. Pellentesque nec nam aliquam sem et tortor consequat id. Dui accumsan sit amet nulla facilisi morbi. Accumsan sit amet nulla facilisi morbi tempus iaculis urna. Semper risus in hendrerit gravida. Porttitor lacus luctus accumsan tortor posuere ac ut consequat. Massa placerat duis ultricies lacus sed turpis. Pellentesque elit ullamcorper dignissim cras tincidunt. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna. Dui ut ornare lectus sit amet est placerat in egestas. Elementum integer enim neque volutpat ac tincidunt vitae. Donec ac odio tempor orci dapibus ultrices in iaculis nunc. Venenatis cras sed felis eget.</p>
+        
+        </div>
+       
+        <div style="margin-top:-5px;">
+
+          <div class="container4">
+            <div class="container"style="margin-top:-20px;">
+                <div style="margin-left:85px;">
+                    <img src="https://img.icons8.com/fluency/48/fa314a/marker.png"/>
+                </div>
+                <span style="color:black;font-size:30px;margin-left:100px;">Address</span>
+                <p style="margin-left:99px;">
+                    House no.XX,Road No.ZY<br>
+                    Uttara,Dhaka-1203,Bangladesh<br>
+                </p>
+            </div>
+            </div>
+            <div class="container" style="margin-left:400px;margin-top:-180px;">
+                <div style="margin-left:85px;">
+                    <img src="https://img.icons8.com/ios-filled/50/fa314a/phone.png"/>
+                  </div>
+                <span style="color:black;font-size:30px;margin-left:100px;">Phone</span>
+                <p style="margin-left:99px;">
+                    01XXXXXXXXX
+                </p>
+            </div>
+            <div class="container" style="margin-left:-100px;margin-top:30px;">
+                <div style="margin-left:800px;margin-top:-200px;">
+                    <img src="https://img.icons8.com/ios-filled/50/fa314a/mail.png"/>
+                </div>
+                <div style="margin-left:700px;margin-top:10px;">
+                <span style="color:black;font-size:30px;margin-left:100px;">Email</span>
+                <p style="margin-left:99px;font-size:20px;margin-top:10px;">
+                   fooddelight@gmail.com 
+                </p>
+                </div>
+            </div>
+                       </div>     
+                       <div class="container4">
+            <div class ="container" style="margin-top:-50px;">
+                <div class="SocialMedia">
+                    <span style="color:black;font-size:30px;margin-left:415px;margin-top:200px;">Follow Us</span>
+                  <div style="margin-left:-100px;">
+                    <ul> 
+                        <div class="SocialMediaListType">
+                        <div class="text-align">
+                         
+                        <li><a href="#"><img src="https://img.icons8.com/color/48/000000/facebook-new.png"/></a></li>
+                        <li><a href="#"><img src="https://img.icons8.com/color/48/000000/instagram-new--v1.png"/></a></li>
+                        <li><a href="#"><img src="https://img.icons8.com/color/48/000000/twitter--v2.png"/></a></li>
+                   
+                      </div>
+                        </div>
+                    
+  
+                      </ul>
+            </div>
+      </div>
+      </div>
+      </div>
+  
+   <div class="container4"></div>
+  
+      <div class="container">
+  
+        <div id="footer">
+            <div class="text-align">
+            Copyright &copy;2021 Rownita Tasneem      
+  
+            </div>
+  
+            </div>
+          </div>
+              
+       
+  
+      </div>
+           
+            
+        </div>
+    </div>
+    </div>
+    </body>
+</html>
---
+++ partials-front/menu.php
@@ -0,0 +1,54 @@
+<?php include('config/constants.php'); ?>
+
+<!DOCTYPE html>
+<html lang="US-en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="description" content="This is a web based food delivery system">
+    <meta name="keywords" content="fooddelight,FOODDELIGHT,Food delight,FOOD DElIGHT,Restaurant,restaurant,web based restaurant">
+    <link  rel="stylesheet" href="css\style.css"   />
+
+     
+
+    <title>Food Delight</title>
+
+    </head>
+
+
+
+
+
+    <body>
+       
+           <div class="container">
+        
+        <div id="Navigation-bar">
+ 
+            <div id="FoodDelightLogo">
+               <img src="homepagephotos\FinalLogo.PNG" alt="Food Delight Logo">
+              
+         
+            </div>
+            </div>
+   
+
+
+                <div class="NavigationLinks Text-Align ">
+                    <ul>
+                        <li><a href="index.php">HOME</a></li>
+                        <li><a href="categories.php">CATEGORIES</a></li>
+                        <li><a href="foods.php">FOODS</a></li>
+                        <li><a href="profile.php">PROFILE</a></li>
+                       <li><a href="order.php">ORDER</a></li>
+                       <li><a href="about.php">ABOUT </a></li>
+                        <li><a href="#">LOGOUT</a></li>
+
+                        </ul>
+                        <div class="clearfix"></div>
+                </div>
+
+         </div>
+
+     
