5ee67640041ec36661f90eb8cd9a1ff1ced3e312
RownitaTasneem <57524665+RownitaTasneem@users.noreply.github.com>
2021-09-08 07:31:56 +0600

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,17 @@
 <?php
+$firstName=$_POST['firstName'];
+$lastName=$_POST['lastName'];
+$gender=$_POST['gender'];
+$email=$_POST['email'];
+$location=$_POST['location'];
+$number=$_POST['number'];
 
+
+//connection code
 $servername="localhost";
 $username="root";
 $dbpass="";
-$dbname="food-delight-1";
+$dbname="food-delight";
 $conn =new mysqli($servername,$username,$dbpass,$dbname);
 if($conn->connect_error){
 
@@ -14,6 +22,12 @@ if($conn->connect_error){
 else{
 
     //echo"connected";
+    $stmt=$conn->prepare("insert into delivery (firstName,lastName,gender,email,location,number) values(?,?,?,?,?,?)");
+    $stmt->bind_param("sssssi",$firstName,$lastName,$gender,$email,$location,$number);
+    $stmt->execute();
+    echo"Submitted Successfully";
+    $stmt->close();
+    $conn->close();
 }
 
 
