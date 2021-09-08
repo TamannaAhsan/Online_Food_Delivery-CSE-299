29d8316cb92851954ad930da903b02cb16464de1
RownitaTasneem <57524665+RownitaTasneem@users.noreply.github.com>
2021-09-08 07:34:32 +0600

    Create delivery.php

---
+++ delivery.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Delivery</title>
+    <!-- Required meta tags -->
+    <meta name="description" content="This is a web based food delivery system">
+    <meta name="keywords" content="fooddelight,FOODDELIGHT,Food delight,FOOD DElIGHT,Restaurant,restaurant,web based restaurant">
+    <link  rel="stylesheet" href="css\style.css"   />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+      
+  <div class="container">
+        
+        <div id="Navigation-bar">
+ 
+            <div id="FoodDelightLogo">
+               <img src="homepagephotos\FinalLogo.PNG" alt="Food Delight Logo">
+              
+         
+            </div>
+            </div>
+   
+
+
+                <div class="NavigationLinks Text-Align ">
+                    <ul>
+                        <li><a href="index.php">HOME</a></li>
+                        <li><a href="categories.php">CATEGORIES</a></li>
+                        <li><a href="foods.php">FOODS</a></li>
+                        <li><a href="order.php">ORDER</a></li>
+                        <li><a href="profile.php">PROFILE</a></li>
+                       <li><a href="delivery.php">DELIVERY</a></li>
+                       <li><a href="about.php">ABOUT </a></li>
+                        <li><a href="#">LOG OUT</a></li>
+
+                        </ul>
+                        <div class="clearfix"></div>
+                </div>
+
+         </div>
+         <div style="margin-left:500px">
+         <div class="container">
+             <div class="row col-md-6 col-md-offset-3">
+                 <div class="panel panel-primary">
+                   <div class="panel-heading text-center">
+
+                   <h1>Delivery Information</h1>
+
+                   </div>
+                    <div class="panel-body">
+                        <form action="connection.php" method="post">
+                          <div class="form-group">
+
+                            <label for="firstName">First Name </label>
+
+                            <input type="text" class="form-control" id="firstName" name="firstName"/>
+
+
+                          </div>
+                          <div class="form-group">
+
+                            <label for="LastName">Last Name </label>
+
+                            <input type="text" class="form-control" id="lastName" name="lastName"/>
+
+
+                          </div>
+                          <div class="form-group">
+
+                            <label for="gender">Gender</label>
+
+                          <div>
+                         <label for="male" class="radio-inline">
+                         <input type="radio" name="gender" id="male" name="gender">
+                         Male
+
+                         </label>
+                         <label for="female" class="radio-inline">
+                         <input type="radio" name="gender" id="female" name="gender">
+                         Female
+
+                         </label>
+                         <label for="others" class="radio-inline">
+                         <input type="radio" name="gender" id="others" name="gender">
+                         Others
+
+                         </label>
+
+                          </div>
+
+                          </div>
+                           <div class="form-group">
+                           <label for="email" >Email</label>
+                           <input type="email" class="form-control" id="email" name="email">
+
+                           </div>
+                           <div class="form-group">
+                            <label for="locatiom" >Location </label>
+                           <input type="text" class="form-control" id="location" name="location"/>
+
+                           </div>
+                           <div class="form-group">
+                               <label for="number">Phone Number </label>
+                            <input type="number" class="form-control" id="number" name="number"/>
+
+                           </div>
+
+                         <input type="submit" class="btn btn-primary">
+
+                        </form>
+                 </div>
+                 </div>
+                 <div class ="container">
+              <div class="SocialMedia">
+                  <h2 class="text-align">Follow Us</h2>
+                  <ul> 
+                      <div class="SocialMediaListType">
+                      <div class="text-align">
+                       
+                      <li><a href="#"><img src="https://img.icons8.com/color/48/fa314a/facebook-circled--v1.png"/></a></li>
+                      <li><a href="#"><img src="https://img.icons8.com/color/48/000000/instagram-new--v1.png"/></a></li>
+                      <li><a href="#"><img src="https://img.icons8.com/color/48/000000/twitter--v2.png"/></a></li>
+                 
+                    </div>
+                      </div>
+                  
+
+                    </ul>
+          
+    </div>
+    </div>
+
+
+                 <div class="panel-footer text-right" style="margin-left:150px;">
+            <small>&copy; Rownita Tasneem</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+ 
+</body>
+</html>
