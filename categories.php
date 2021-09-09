8df51c07acf5ac9abd921b799c85b39243ea91c7
RownitaTasneem <57524665+RownitaTasneem@users.noreply.github.com>
2021-09-09 06:03:22 +0600

    Add files via upload

---
+++ categories.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="description" content="This is a web based food delivery system">
+    <meta name="keywords" content="fooddelight,FOODDELIGHT,Food delight,FOOD DElIGHT,Restaurant,restaurant,web based restaurant">
+    <link  rel="stylesheet" href="css\style.css"   />
+     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+     
+
+    <title>Categories</title>
+</head>
+
+
+
+
+<body>
+  <header>
+    <div class="container">
+        
+        <div id="Navigation-bar">
+ 
+            <div id="FoodDelightLogo">
+               <img src="homepagephotos\FinalLogo.PNG" alt="Food Delight Logo">
+              
+         
+            </div>
+            </div>
+   
+
+
+                <div class="NavigationLinks Text-Align ">
+                    <ul>
+                        <li><a href="index.php">HOME</a></li>
+                        <li><a href="categories.php">CATEGORIES</a></li>
+                        <li><a href="foods.php">FOODS</a></li>
+                        <li><a href="order.php">ORDER</a></li>
+                        <li><a href="profile.php">PROFILE</a></li>
+                        <li><a href="delivery.php">DELIVERY</a></li>
+                        <li><a href="about.php">ABOUT</a></li>
+                        <li><a href="#">LOG OUT</a></li>
+
+                        </ul>
+                        <div class="clearfix"></div>
+                </div>
+
+         </div>
+         </header>
+         <main>
+         <div class="Cuisines">
+  
+            <h2 class="text-align" style="margin-bottom:-200px;" ><a href="categories.php">Categories</a></h2>
+           
+          <div class="image_style">
+               <img src= "homepagephotos\BangladeshiCuisine.jpg"  alt="Bangladeshi Cuisine" class="ResponsiveImage" style="width:200px;height:200px;border-radius: 50px;margin-left:120px;" >
+             
+          
+                   <img src= "homepagephotos\Italian-Cuisine.jpg"  alt="Italian Cuisine" class="ResponsiveImage"  style="width:200px;height:200px;border-radius:50px;margin-left:100px;">
+        
+                       <img src= "homepagephotos\ChineseCuisine.jpg"  alt="Chinese Cuisine" class="ResponsiveImage"  style="width:200px;height:200px;border-radius:50px;margin-left:100px;" >
+                       <img src= "homepagephotos\American-foods.jpg"  alt="American Cuisine" class="ResponsiveImage"  style="width:200px;height:200px;border-radius:50px;margin-left:100px;" >
+              </div>
+              <div class="styling_text">
+                <pre>
+              <i style="font-size:30px ;color:black;margin-top:10px;">  Bangladeshi          Italian             Chinese            American  </i>
+               </pre>
+            </div>
+             
+              <div class="image_style"  style="margin-top:-30px;">
+                <img src= "foodrecipies/Iranian.jpeg"  alt="iranian Cuisine" class="ResponsiveImage" style="width:200px;height:200px;border-radius: 50px;margin-left:120px;" >
+              
+               <img src= "foodrecipies/Swedish.jpg"  alt="Swedish Cuisine" class="ResponsiveImage"  style="width:200px;height:200px;border-radius:50px;margin-left:100px;">
+         
+                        <img src= "foodrecipies/Mexican-Cuisine.jpg"  alt="Mexican Cuisine" class="ResponsiveImage"  style="width:200px;height:200px;border-radius:50px;margin-left:120px;" >
+                        <img src= "foodrecipies/thai_cuisine.jpg"  alt="Thai Cuisine" class="ResponsiveImage"  style="width:200px;height:200px;border-radius:50px;margin-left:120px;" >
+               </div>
+                       
+                       
+               <div class="clearfix"></div> <!--Stopping float of boxes-->
+
+           </div>
+           </div>
+        
+           <div class="styling_text">
+            <pre>
+          <i style="font-size:30px ;color:black;margin-top:10px;">  Iranian             Swedish                Mexican              Thai  </i>
+           </pre>
+        </div>
+         
+
+         <div class ="container">
+            <div class="SocialMedia">
+                <h2 class="text-align">Follow Us</h2>
+                <ul> 
+                    <div class="SocialMediaListType">
+                    <div class="text-align">
+                     
+                    <li><a href="#"><img src="https://img.icons8.com/color/48/fa314a/facebook-circled--v1.png"/></a></li>
+                    <li><a href="#"><img src="https://img.icons8.com/color/48/000000/instagram-new--v1.png"/></a></li>
+                    <li><a href="#"><img src="https://img.icons8.com/color/48/000000/twitter--v2.png"/></a></li>
+               
+                  </div>
+                    </div>
+                
+
+                  </ul>
+        
+  </div>
+  </div>
+  </main>
+
+  <div class="container">
+
+    <div id="footer">
+        <div class="text-align">
+        Copyright &copy;2021 Rownita Tasneem      
+
+        </div>
+
+        </div>
+      </div>
+
+
+
+    </body>
+
+</html>
