e57863c654d9caea6247000b1b055909cd12544c
Brishty <52675761+TamannaAhsan@users.noreply.github.com>
2021-09-14 03:24:24 +0600

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,74 +1,49 @@
-<?php
+<?php include('partials-front/menu.php');
 
- include("header.php");
- 
-    if ($_SERVER["REQUEST_METHOD"] =="POST") {
-        if (isset($_POST['submit'])) {
-            if (isset($_SESSION["cart"])) {
-                $myitems = array_column($_SESSION["cart"], 'item_name');
-                if (in_array($_POST["item_name"], $myitems)) {
-                    echo "Item already added";
-                    $_SESSION['exist-item'] ="<div><b>Item already added</b></div>";
-                    //Redirect to My Cart Page
-                    header("location:".SITE.'mycart.php');
-                } else {
-                    $count = count($_SESSION["cart"]);
-                    ($_SESSION["cart"])[$count]=array('item_name'=>$_POST['item_name'],'item_price'=> $_POST['item_price'],'qty'=>1 );
-                   
-                    $_SESSION['add-item'] ="<div><b>Item added</b></div>";
-                    //Redirect to My Cart Page
-                    header("location:".SITE.'mycart.php');
-                }
-            } else {
-                $_SESSION["cart"][0]=array('item_name'=>$_POST['item_name'],'item_price'=> $_POST['item_price'],'qty'=>1 );
-                
-                $_SESSION['add-item'] ="<div><b>Item added</b></div>";
-                //Redirect to My Cart Page
-                header("location:".SITE.'mycart.php');
-            }
-        }
-    }
+ ?>
 
-?>
-
-<head>
-    <title>Cart</title>
-</head>
-
-
-<body>
-    <div class="container mt-5 " style="width: 800px">
-        <h3 align="center">Food Cart</h3>
-
-        <?php
-
-        $sql = "SELECT * FROM tbl_food ORDER BY id ASC";
-        $res = mysqli_query($conn, $sql);
-
-            if (mysqli_num_rows($res)>0) {
-                while ($row = mysqli_fetch_array($res)) {
-                    ?>
-        <div class="col-md-3">
-            <form method="POST" action="">
-
-                <div style: "border: 1px solid #333; background-color: #f1f1f1; border-radius:5px; padding:16px;"
-                    align="center"></div><br><br>
-
-                <img src="<?php echo SITE; ?>images/food/<?= $row['image_name']; ?>" alt="Pizza"
-                    class="img-responsive img-curve">
-                <h4 class=" text-info"><?php echo $row["title"]; ?></h4>
-                <h4 class="text-danger"><?php echo $row["price"]; ?></h4>
-                <br>
-                <button type="submit" name="submit" class="btn btn-primary">Add to cart</button>
-                <input type="hidden" name="item_name" value="<?php echo $row["title"]; ?>">
-                <input type="hidden" name="item_price" value="<?php echo $row["price"]; ?>">
-        </div>
-        <?php
-                }
-            }
-            
-?>
-    </div>
-</body>
+<div class="container" style="text-align:center">
+    <h1>Cart</h1>
+    <?php if ($_SESSION["shopping_cart"]) :?>
+    <table class="table table-bordered">
+        <tr>
+            <th width="40%">Item Name</th>
+            <th width="10%">Quantity</th>
+            <th width="20%">Price</th>
+            <th width="15%">Total</th>
+            <th width="5%">Action</th>
+        </tr>
+        <?php   
+                if(!empty($_SESSION["shopping_cart"]))  
+                {  
+                    $total = 0;  
+                    foreach($_SESSION["shopping_cart"] as $keys => $values)  
+                    {  
+                ?>
+        <tr>
+            <td><?php echo $values["product_name"]; ?></td>
+            <td><?php echo $values["product_quantity"]; ?></td>
+            <td>$ <?php echo $values["product_price"]; ?></td>
+            <td>$ <?php echo number_format($values["product_quantity"] * $values["product_price"], 2); ?></td>
+            <td><a href="index.php?action=delete&product_id=<?php echo $values["product_id"]; ?>"><span
+                        class="text-danger">Remove</span></a></td>
+        </tr>
+        <?php  
+                        $total = $total + ($values["product_quantity"] * $values["product_price"]);  
+                    }  
+                ?>
+        <tr>
+            <td colspan="3" align="right">Total</td>
+            <td align="right">$ <?php echo number_format($total, 2); ?></td>
+            <td></td>
+        </tr>
+        <?php  
+                }  
+                ?>
+    </table>
+    <?php else: ?>
+    <h1>Cart is empty</h1>
+    <?php endif; ?>
+</div>
 
 <?php include('partials-front/footer.php'); ?>
