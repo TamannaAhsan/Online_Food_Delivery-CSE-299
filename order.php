5ac7a703af3be32932b3999a263e909b99e4ff5c
RownitaTasneem <57524665+RownitaTasneem@users.noreply.github.com>
2021-09-14 07:35:15 +0600

    Update order.php

--- order.php
+++ order.php
@@ -43,7 +43,8 @@
                         <li><a href="foods.php">FOODS</a></li>
                         <li><a href="order.php">ORDER</a></li>
                         <li><a href="profile.php">PROFILE</a></li>
-                        <li><a href="delivey.php">DELIVERY</a></li>
+                             <li><a href="location.php">LOCATION</a></li>
+                       <li><a href="contact.php">CONTACT</a></li>
                         <li><a href="about.php">ABOUT</a></li>
                         <li><a href="#">LOG OUT</a></li>
                      
