91e99518ffde152a5a1d456717aa3fde69c3402b
RownitaTasneem <57524665+RownitaTasneem@users.noreply.github.com>
2021-09-14 07:33:47 +0600

    Update foods.php

--- foods.php
+++ foods.php
@@ -37,8 +37,9 @@
                         <li><a href="foods.php">FOODS</a></li>
                         <li><a href="order.php">ORDER</a></li>
                         <li><a href="profile.php">PROFILE</a></li>
-                        <li><a href="delivery.php">DELIVERY</a></li>
-                        <li><a href="contact.php">CONTACT</a></li>
+                             <li><a href="location.php">LOCATION</a></li>
+                       <li><a href="contact.php">CONTACT</a></li>
+                      
                         <li><a href="#">LOG OUT</a></li>
 
                         </ul>
