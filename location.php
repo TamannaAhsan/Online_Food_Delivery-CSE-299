4645e65fb7a136902206c59a543408c41359cb78
RownitaTasneem <57524665+RownitaTasneem@users.noreply.github.com>
2021-09-14 07:18:05 +0600

    Update and rename delivery.php to location.php

--- location.php
+++ location.php
@@ -1,16 +1,25 @@
 
-
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Delivery</title>
+    <title>Location</title>
     <!-- Required meta tags -->
     <meta name="description" content="This is a web based food delivery system">
     <meta name="keywords" content="fooddelight,FOODDELIGHT,Food delight,FOOD DElIGHT,Restaurant,restaurant,web based restaurant">
     <link  rel="stylesheet" href="css\style.css"   />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-     
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+    crossorigin=""/>
+     <!-- Make sure you put this AFTER Leaflet's CSS -->
+ <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+ integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+ crossorigin=""></script>
+      <style>
+          #mapid { height: 200px; }
+
+      </style>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
@@ -34,9 +43,10 @@
                         <li><a href="index.php">HOME</a></li>
                         <li><a href="categories.php">CATEGORIES</a></li>
                         <li><a href="foods.php">FOODS</a></li>
-                        <li><a href="order.php">ORDER</a></li>
+                      
                         <li><a href="profile.php">PROFILE</a></li>
-                       <li><a href="delivery.php">DELIVERY</a></li>
+                       <li><a href="location.php">LOCATION</a></li>
+                       <li><a href="contact.php">CONTACT</a></li>
                        <li><a href="about.php">ABOUT</a></li>
                         <li><a href="#">LOG OUT</a></li>
 
@@ -45,15 +55,13 @@
                 </div>
 
          </div>
-                   
-
-                    <div style="margin-left:500px">
+         <div style="margin-left:500px">
          <div class="container">
              <div class="row col-md-6 col-md-offset-3">
                  <div class="panel panel-primary">
                    <div class="panel-heading text-center">
 
-                   <h1>Delivery Information</h1>
+                   <h1>Your  Information & Check Our Location</h1>
 
                    </div>
                     <div class="panel-body">
@@ -142,11 +150,10 @@
                             <option value="Rangpur" class="form-control" id="city" name="city">Rangpur</option>
                             <option value="Comilla" class="form-control" id="city" name="city">Comilla</option>
                             <option value="Narayanganj" class="form-control" id="city" name="city">Narayanganj</option>
-                        
+                            <option value="Comilla" class="form-control" id="city" name="city">Comilla</option>
      
 
                             </select>
-                           
                            </div>
                            <div class="form-group">
                                <label for="number">Phone Number </label>
@@ -160,12 +167,66 @@
     
                            </div>
 
-                         <input type="submit" class="btn btn-primary" value="Get Delivery">
+                         <input type="submit" class="btn btn-primary" value="Send" style="margin-bottom:10px;">
 
                         </form>
                  </div>
                  </div>
-                 <div class ="container">
+                 <div class="container"id="mapid">
+           
+
+           </div>
+        
+    <div id="mapid" style="width: 800px; height: 500px;margin-top:100px;margin-left:-200px;"></div>
+    <script>
+    
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    
+      L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1
+      }).addTo(mymap);
+    
+      L.marker([51.5, -0.09]).addTo(mymap)
+        .bindPopup("<b>Here is location of food-delight!</b><br />.").openPopup();
+    
+      L.circle([51.508, -0.11], 500, {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5
+      }).addTo(mymap).bindPopup("I am a circle.");
+    
+      L.polygon([
+        [51.509, -0.08],
+        [51.503, -0.06],
+        [51.51, -0.047]
+      ]).addTo(mymap).bindPopup("I am a polygon.");
+    
+    
+      var popup = L.popup();
+    
+      function onMapClick(e) {
+        popup
+          .setLatLng(e.latlng)
+          .setContent("You clicked the map at " + e.latlng.toString())
+          .openOn(mymap);
+      }
+    
+      mymap.on('click', onMapClick);
+    
+    </script>
+                  <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+              
+
+    <div class ="container" style="margin-top:-600px;">
               <div class="SocialMedia">
                   <h2 class="text-align">Follow Us</h2>
                   <ul> 
@@ -179,14 +240,14 @@
                     </div>
                       </div>
                   
-
                     </ul>
           
     </div>
     </div>
 
 
-    <div class="container">
+
+    <div class="container" style="margin-top:-500px;">
 
 <div id="footer">
     <div class="text-align">
