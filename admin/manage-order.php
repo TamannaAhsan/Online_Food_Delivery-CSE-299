ec665aaf61ad3d33d16fde8327b1020ed5216c04
Brishty <52675761+TamannaAhsan@users.noreply.github.com>
2021-09-17 12:22:54 +0600

    Update manage-order.php

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -18,75 +18,69 @@
         <br>
 
 
-        <table class="content-table">
+        <table class="content-table w-100">
             <thead>
 
                 <tr>
                     <th>Serial no</th>
-                    <th>Food item </th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Total</th>
+                    <th>Order id</th>
                     <th>Order Date</th>
-                    <th>Status</th>
                     <th>Customer Name</th>
                     <th>Contact</th>
                     <th>Email</th>
                     <th>Address</th>
+                    <th>Status</th>
                     <th>Actions</th>
 
                 </tr>
             </thead>
             <?php
 
-    $sql = "SELECT * FROM tbl_order ORDER BY id DESC"; // Display the latest order at first
+    $sql = "SELECT * FROM tbl_order ORDER BY order_id DESC"; // Display the latest order at first
     $res = mysqli_query($conn, $sql);
     $count = mysqli_num_rows($res);
     $sn=1;
 
     if ($count>0) {
         while ($row=mysqli_fetch_assoc($res)) {
-            $id = $row['id'];
-            $food = $row['food'];
-            $price = $row['price'];
-            $qty = $row['qty'];
-            $total = $row['total'];
-            $order_date = $row['order_date'];
-            $status = $row['status'];
+            $id = $row['order_id'];
+             $date = $row['date'];
+             $status= $row['status'];
             $customer_name = $row['customer_name'];
-            $customer_contact = $row['customer_contact'];
+            $customer_contact = $row['customer_mobile'];
             $customer_email = $row['customer_email'];
             $customer_address = $row['customer_address']; ?>
 
             <tr>
                 <td><?php echo $sn++; ?></td>
-                <td><?php echo $food; ?></td>
-                <td><?php echo $price; ?></td>
-                <td><?php echo $qty; ?></td>
-                <td><?php echo $total; ?></td>
-                <td><?php echo $order_date; ?></td>
+                <td><?php echo $id; ?></td>
+                <td><?php echo $date; ?></td>
+                <td><?php echo $customer_name; ?></td>
+                <td><?php echo $customer_email; ?></td>
+                <td><?php echo $customer_contact; ?></td>
+
+                <td><?php echo $customer_address; ?></td>
                 <td>
                     <?php
-                if ($status=="Ordered") {
-                    echo "<label style='color:blue;'>$status</label>";
-                } elseif ($status=="On Delivery") {
-                    echo "<label style='color:orange;'>$status</label>";
-                } elseif ($status=="Delivered") {
-                    echo "<label style='color:green;'>$status</label>";
-                } elseif ($status=="Cancelled") {
-                    echo "<label style='color:red;'>$status</label>";
-                } ?>
+                        if ($status=="Ordered") {
+                            echo "<label style='color:blue;'>$status</label>";
+                        } elseif ($status=="On Delivery") {
+                            echo "<label style='color:orange;'>$status</label>";
+                        } elseif ($status=="Delivered") {
+                            echo "<label style='color:green;'>$status</label>";
+                        } elseif ($status=="Cancelled") {
+                            echo "<label style='color:red;'>$status</label>";
+                        } ?>
 
 
                 </td>
-                <td><?php echo $customer_name; ?></td>
-                <td><?php echo $customer_contact; ?></td>
-                <td><?php echo $customer_email; ?></td>
-                <td><?php echo $customer_address; ?></td>
-
                 <td>
-                    <a href="<?php echo SITE; ?>admin/update-order.php?id=<?php echo $id; ?>"
-                        class="btn-update">Update</a> <br><br>
+                    <a href="<?php echo SITE; ?>admin/view-order.php?id=<?php echo $id; ?>">Order
+                        details</a>
+                    <br><br>
+                    <a href="<?php echo SITE; ?>admin/edit-order.php?id=<?php echo $id; ?>" class="btn-update">Edit</a>
+                    <br><br>
+
                     <a href="<?php echo SITE; ?>admin/delete-order.php?id=<?php echo $id; ?>"
                         class="btn-detele">Delete</a>
 
